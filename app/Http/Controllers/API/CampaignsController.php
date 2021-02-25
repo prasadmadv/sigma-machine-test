aa92d9eba86682cae59976da71d25e01ee4ffe43
prasadmadv <mad9prasad@gmail.com>
2021-02-25 09:22:26 +0530

    reports data created, campaign_id, user_email and clicks of a user, per campaign

--- app/Http/Controllers/API/CampaignsController.php
+++ app/Http/Controllers/API/CampaignsController.php
@@ -6,18 +6,30 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Campaign;
 use App\Models\Report;
+use DB;
 
 class CampaignsController extends Controller
 {
     public function showReport()
     {
-        $items = Report::select('reports.*','users.username as username','campaigns.name as campaign_name')
-        ->leftJoin('users','users.email','reports.user_email')
-        ->leftJoin('campaigns','campaigns.id','reports.campaign_id')
-        ->get()
-        ->toArray();
-        dd($items);
-        return $items;
+        $select = [
+            'reports.campaign_id as campaign_id', 
+            'reports.user_email as user_email',
+            'users.username as username',
+            'users.id as user_id',
+            'campaigns.name as campaign_name', 
+            DB::raw('count(*) as clicks'),
+            DB::raw("CONCAT(reports.campaign_id,'-',users.id) as id"),
+        ];
+        $reports = DB::table('reports')
+                   ->select($select)
+                    ->leftJoin('users','users.email','reports.user_email')
+                    ->leftJoin('campaigns','campaigns.id','reports.campaign_id')
+                    ->groupBy('campaign_id','user_email')
+                    ->get()
+                    ->toArray();
+        return $reports;
+
     }
     public function index()
     {
